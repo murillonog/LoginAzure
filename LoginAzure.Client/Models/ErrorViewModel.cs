43ddf4da3b368605085a51a593973efc07b87df7
Murillo Nogueira <murillo.nogueira01@outlook.com>
2022-12-12 22:41:20 -0300

    Adicionar arquivos de projeto.

---
+++ LoginAzure.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginAzure.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
