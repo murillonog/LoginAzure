1e7fabf698512e377fd6e0e6f35ace4a209cb47f
Murillo Nogueira <murillo.nogueira01@outlook.com>
2022-12-14 16:24:36 -0300

    Criação e configuração da API

---
+++ LoginAzure.Api/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginAzure.Api.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Owner { get; set; }
+    }
+}
